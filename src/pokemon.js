3da7452b300517039fc6c0466da29a6ba39a079a
usagi.f@gmail.com <usagi.f@gmail.com>
2017-07-20 16:29:55 +0900

    update: async/await構文で書いてみる

--- src/pokemon.js
+++ src/pokemon.js
@@ -3,6 +3,7 @@
  * `get pokemon` というコマンドで、botがランダムに一匹のポケモンを捕獲してきます。
 */
 
+import 'babel-polyfill';
 import fetch from 'node-fetch';
 import translateData from '../data/pokemon.json';
 import * as libs from './pokemon/libs';
@@ -11,21 +12,21 @@ import { MAX, RES } from './pokemon/constants';
 module.exports = (robot) => {
     robot.respond(/get pokemon/, (res) => {
         res.send(RES.go);
-
         const randomUrl = libs.getRandomUrl(MAX);
-        fetch(randomUrl)
-            .then(response => {
-                if (response.status !== 200) res.send(RES.miss);
-                return response.json();
-            })
-            .then(json => {
+        (async () => {
+            try {
+                const response = await fetch(randomUrl);
+
+                const status = response.status;
+                if (status !== 200) res.send(RES.miss);
+
+                const json = await response.json();
                 const pokeData = libs.getPokeData(json);
                 res.send(libs.getSuccessRes(pokeData));
                 res.send(libs.evalPokeCpRes(pokeData.cp));
-            })
-            .catch(err => {
-                throw new Error('API response is invalid');
-                res.send(RES.miss);
-            });
+            } catch(err) {
+                res.send(err);
+            }
+        })()
     });
 }
