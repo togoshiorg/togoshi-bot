2bf50aa1a906860e848c025b42b0a87ddd47f8ce
usagi.f@gmail.com <usagi.f@gmail.com>
2017-07-24 20:30:31 +0900

    update: child_addedでlistを購読する
    timestampで初回は走らないように

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,7 @@
 // @flow
 
 import * as firebase from 'firebase';
+import { isAfter, format } from 'date-fns';
 
 // initialize firebase
 const app = firebase.initializeApp({
@@ -11,11 +12,24 @@ const app = firebase.initializeApp({
 });
 
 const getlist = app.database().ref('/getlist');
+const tmp = app.database().ref('/tmp');
+const initialize = (tmp, getlist) => {
+    const starttime = format(new Date(), 'YYYY-MM-DDTHH:mm:ssZ');
+    tmp.set({ starttime });
+
+    tmp.once('value', tSnapshot => {
+        const STARTUP_TIMESTAMP = tSnapshot.val().timestamp;
+        getlist.on('child_added', mSnapshot => {
+            const snapshotData = mSnapshot.val();
+            if (isAfter(snapshotData.time, STARTUP_TIMESTAMP)) {
+                console.log(snapshotData);
+            }
+        });
+    });
+};
+
+initialize(tmp, getlist);
 
 export const pushData = (data: Object) => {
     getlist.push(data);
 };
-
-// getlist.on('value', snapshot => {
-//     console.log(snapshot.val());
-// });
