db8aac84254c9f302aa03b54694aaa600d3c055a
Mutsumi Ito <m.ito@loose-art.com>
2017-08-29 16:46:36 +0900

    fix: レビューの修正
    - 大部分をletからconstに変更
    - 部屋判定と雑談判定を一つの関数にまとめた
    - APIのステータスエラーの処理共通化

--- src/chat.js
+++ src/chat.js
@@ -12,14 +12,13 @@ module.exports = (robot) => {
     const chatApi = 'https://chatbot-api.userlocal.jp/api/chat';
     const charaApi = 'https://chatbot-api.userlocal.jp/api/character';
     const errMsg = 'APIエラーゴシ・・・';
-
-    let key = process.env.HUBOT_USERLOCAL_API_KEY;
-    let chatRoom = roomJson.hubot;
-    let roomJudge = (res) => {
-        const resRoom = res.message.user.room;
-        return resRoom === chatRoom;
+    const key = process.env.HUBOT_USERLOCAL_API_KEY;
+    const chatRoom = roomJson.hubot;
+    const chatJudge = (res, bool) => {
+        bool = chatTerms || !chatTerms;
+        return res.message.user.room === chatRoom && bool;
     };
-    let apiUrl = (api, paramsArr) => {
+    const apiUrl = (api, paramsArr) => {
         let url = api;
         for (let key in paramsArr) {
             url += (url.indexOf('?') === -1) ? '?' : '&';
@@ -27,11 +26,12 @@ module.exports = (robot) => {
         };
         return url;
     };
+
     let chatTerms = false;
 
     // 雑談開始
     robot.respond(/お話しよう/, (res) => {
-        if (roomJudge(res) && !chatTerms) {
+        if (chatJudge(res, chatTerms)) {
             res.send('いいゴシよ！');
             res.finish();
             chatTerms = true;
@@ -40,7 +40,7 @@ module.exports = (robot) => {
 
     // 雑談終了
     robot.hear(/お話おしまい/, (res) => {
-        if (roomJudge(res) && chatTerms) {
+        if (chatJudge(res, chatTerms)) {
             res.send('楽しかったゴシ！またお話しようゴシ〜');
             res.finish();
             chatTerms = false;
@@ -49,33 +49,30 @@ module.exports = (robot) => {
 
     // 雑談中
     robot.hear(/(.*)/i, (res) => {
-        if (roomJudge(res) && chatTerms) {
+        if (chatJudge(res, chatTerms)) {
             // 本来は.http(url).query(queryParameter).get()でAPIたたけるけど勉強のため今回はあえてasyncで
-            let params = (msg) => {
-                let obj = {
+            const params = (msg) => {
+                return {
                     'key': key,
                     'message': encodeURIComponent(msg),
                     'character_type': 'custom'
                 };
-                return obj;
             };
-            let asyncApi = async (api, resMsg) => {
+            const asyncApi = async (api, resMsg) => {
                 try {
-                    let response = await fetch(apiUrl(api, params(resMsg)));
-                    let status = response.status;
-                    if (status !== 200) {
-                        return errMsg;
-                    }
-                    let data = await response.json();
+                    const response = await fetch(apiUrl(api, params(resMsg)));
+                    const status = response.status;
+                    if (status !== 200) throw new Error(response.statusText);
+                    const data = await response.json();
                     return data.result;
                 } catch (err) {
-                    return errMsg;
+                    return errMsg + err;
                 }
             };
             // 自動会話APIとキャラクター会話変換APIが別のため2回叩く
             (async () => {
-                let midRes = await asyncApi(chatApi, res.match[1]);
-                let endRes = await asyncApi(charaApi, midRes);
+                const midRes = await asyncApi(chatApi, res.match[1]);
+                const endRes = await asyncApi(charaApi, midRes);
                 res.send(endRes);
             })();
         }
