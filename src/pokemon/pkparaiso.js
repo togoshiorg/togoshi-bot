16df26fdceb9bce646f88c91da9df80f91bcd11a
usagi.f@gmail.com <usagi.f@gmail.com>
2017-12-26 19:36:48 +0900

    update: create local name

--- src/pokemon/pkparaiso.js
+++ src/pokemon/pkparaiso.js
@@ -15,36 +15,36 @@ const PATH: Object = {
 };
 
 // 名前変換が必要なポケモンの種類分け
-// const CHANGE_NAME_ARR: Object = {
-//     deleteHyphen: [ // ハイフンのみを削除
-//         29, // ニドラン♀
-//         32 // ニドラン♂
-//     ],
-//     deleteHyphenBack: [ // ハイフンから後ろを削除
-//         386, // デオキシス
-//         413, // ミノマダム
-//         487, // ギラティナ
-//         492, // シェイミ
-//         550, // バスラオ
-//         555, // ヒヒダルマ
-//         641, // トルネロス
-//         642, // ボルドロス
-//         645, // ランドロス
-//         647, // ケルディオ
-//         648, // メロエッタ
-//         678, // ニャオニクス
-//         681, // ギルガルド
-//         710, // バケッチャ
-//         711 // パンプジン
-//     ]
-// };
+const CHANGE_NAME_ARR: Object = {
+    replaceHyphen: [ // ハイフンをアンダースコアに変換
+        29, // ニドラン♀
+        32 // ニドラン♂
+    ],
+    deleteHyphenBack: [ // ハイフンから後ろを削除
+        386, // デオキシス
+        413, // ミノマダム
+        487, // ギラティナ
+        492, // シェイミ
+        550, // バスラオ
+        555, // ヒヒダルマ
+        641, // トルネロス
+        642, // ボルドロス
+        645, // ランドロス
+        647, // ケルディオ
+        648, // メロエッタ
+        678, // ニャオニクス
+        681, // ギルガルド
+        710, // バケッチャ
+        711 // パンプジン
+    ]
+};
 
 export default class Pkparaiso {
     id: number; // ポケモンid
     name: string; // ポケモンname
     isShiny: boolean; // 色違い
     genPath: string; // 世代判定
-    pokestudiumName: string; // pokestudium.com用の名前
+    pkparaisoName: string; // pokestudium.com用の名前
 
     constructor ({ id, name, isShiny }: Object = {}) {
         if (id == null || name == null || isShiny == null) throw new Error('Invalid constructor argument.');
@@ -52,19 +52,20 @@ export default class Pkparaiso {
         this.name = name;
         this.isShiny = isShiny;
         this.genPath = this.createGenerationPath();
-        // this.pokestudiumName = this.createPokestudiumName();
+        this.pkparaisoName = this.createPkparaisoName();
     }
 
-    // pokestudium.com用の名前を作成する
+    // pkparaiso.com用の名前を作成する
+    // ※性別があるニドランは'-'を'_'に変換する。
     // ※形態変化があるポケモンはPokeAPIでは名前の後ろに'-'がついて画像名にそのまま使えない。
-    // createPokestudiumName (): string {
-    //     if (CHANGE_NAME_ARR.deleteHyphen.indexOf(this.id) !== -1) {
-    //         return this.name.replace(/-/, '');
-    //     } else if (CHANGE_NAME_ARR.deleteHyphenBack.indexOf(this.id) !== -1) {
-    //         return this.name.replace(/(-)(.*)/, '');
-    //     }
-    //     return this.name;
-    // }
+    createPkparaisoName (): string {
+        if (CHANGE_NAME_ARR.replaceHyphen.indexOf(this.id) !== -1) {
+            return this.name.replace(/-/, '_');
+        } else if (CHANGE_NAME_ARR.deleteHyphenBack.indexOf(this.id) !== -1) {
+            return this.name.replace(/(-)(.*)/, '');
+        }
+        return this.name;
+    }
 
     // idが722以降の場合は第7世代用のPathになる
     createGenerationPath (): string {
@@ -74,6 +75,6 @@ export default class Pkparaiso {
     // 画像パスを返却する（public）
     getImgPath (): string {
         const shinyPath = this.isShiny ? PATH.shiny : '';
-        return `${PATH.url.intro}${this.genPath}${PATH.url.outro}${shinyPath}/${this.name}.${PATH.fileType}`;
+        return `${PATH.url.intro}${this.genPath}${PATH.url.outro}${shinyPath}/${this.pkparaisoName}.${PATH.fileType}`;
     }
 }
