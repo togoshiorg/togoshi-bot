4f2e7a770c9a4084911e6047bafb5c0fb9be2994
loose-art <m.ito@loose-art.com>
2018-02-08 19:03:53 +0900

    fix: 占い機能の修正
    - requestの仕様変更による、引数部分の修正

--- src/horoscope.js
+++ src/horoscope.js
@@ -20,7 +20,7 @@ module.exports = (robot) => {
         const todayStr = todayDate.getFullYear() + '/' + (('0' + (todayDate.getMonth() + 1)).slice(-2)) + '/' + (('0' + todayDate.getDate()).slice(-2));
         const url = api + todayStr;
 
-        request.get({url, json: true}, (response, body) => {
+        request.get({ url: url, json: true }, (err, response, body) => {
             if (response.statusCode === 200) {
                 const data = body.horoscope[todayStr];
 
@@ -50,12 +50,13 @@ module.exports = (robot) => {
                     three: '*3位 ' + result[2].sign + '*\n' + '金運：' + result[2].money + '　仕事運：' + result[2].job + '　恋愛運：' + result[2].love + '　総合運：' + result[2].total + '\n' + result[2].content
                 };
 
-                robot.send(room, text.title);
-                robot.send(room, text.one);
-                robot.send(room, text.two);
                 robot.send(room, text.three);
+                robot.send(room, text.two);
+                robot.send(room, text.one);
+                robot.send(room, text.title);
             } else {
                 robot.send(room, '今日は占いはお休みゴシ');
+                robot.send(room, err);
             }
         });
     }, null, true, 'Asia/Tokyo');
